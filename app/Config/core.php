d72c3a0519f7d98e4c17260dc123ae7eb872382e
Nobuyuki Morii <qwerty.poiu.mory@gmail.com>
2015-11-22 10:27:04 +0900

    setting Security.salt and Security.cipherSeed

--- app/Config/core.php
+++ app/Config/core.php
@@ -220,13 +220,17 @@
 
 /**
  * A random string used in security hashing methods.
+ * 2015/11/22
+ * nmorii rewrite this
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Djghdj587589hfjashgljkashglja4062306529369');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
+ * 2015/11/22
+ * nmorii rewrite this
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '6273672039670293769823678920746026');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).

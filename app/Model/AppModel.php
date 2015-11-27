c999e078ec0ad949110a0468e49aa81b27baf189
Nobuyuki Morii <qwerty.poiu.mory@gmail.com>
2015-11-27 17:08:05 +0900

    app/Config/define.php：del_flgの定数を数値から文字列に変更 app/Model/AppModel.php：afterfindに以下機能を実装 ・model名を削除 ・削除済みレコードを削除 ・del_flgカラムの値を削除する ・主キーを配列のキーとする

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -30,4 +30,92 @@ App::uses('Model', 'Model');
  * @package       app.Model
  */
 class AppModel extends Model {
+
+    /*
+     * Findの検索結果を変換
+     * ・model名を削除
+     * ・削除済みレコードを削除
+     * ・del_flgカラムの値を削除する
+     * ・主キーを配列のキーとする
+     */
+    public function afterFind($results, $primary = false) {
+
+        /* --------------- 配列の構造を判定する ------------------------------- */
+
+        if(isset($results[0][0])){
+
+            /* Countで取得された場合 */
+            $data_type = "array_double_count";
+
+        } else if(count($results) === 1){
+
+            /* Findの結果が１つのキーを持つ配列の場合 */
+            $data_type = "array_cnt_one";
+
+        } else if(count($results) > 1){
+
+            /* Findの結果が複数のキーを持つ配列の場合 */
+            $data_type = "array_cnt_multiple";
+
+        }
+
+
+        /* --------------- Countで取得された場合 ------------------------------ */
+        if($data_type === "array_double_count"){
+            /* 引数と同じ値を返却 */
+            return $results;
+
+        }
+
+        /* --------------- Findの結果が１つor複数のキーを持つ配列の場合 ---------- */
+        /* Findの戻り値からモデル名を削除 */
+        $results = Hash::extract($results,'{n}.'.$this->name);
+
+
+        /* --------------- Findの結果が１つのキーを持つ配列の場合 --------------- */
+        if($data_type === "array_cnt_one"){
+
+             /* 削除済みレコードの場合 */
+             if($results[0]['del_flg'] === Configure::read("RECORD_DELETED")){
+                /* 空の配列を返却 */
+                return array();
+             }
+
+             /* キーのdel_flgとその値を削除する */
+             unset($results[0]['del_flg']);
+
+            return $results;
+        }  
+
+
+        /* --------------- Findの結果が複数のキーを持つ配列の場合 --------------- */
+        foreach ($results as $key => $value) {
+
+            /* 削除済みレコードを削除する */
+            if(array_key_exists('del_flg', $value)){
+
+                 /* 削除済みのレコードの場合 */
+                if($value['del_flg'] === Configure::read("RECORD_DELETED")){
+                    /* 処理をスキップする */
+                    continue;
+                }
+
+                /* キーのdel_flgとその値を削除する */
+                unset($value['del_flg']);
+            }
+
+            /* 返却値のキーを、カラムのidに変更する */
+            //主キーを設定
+            $primary_key = $this->primaryKey;
+
+            //主キーをキーとして、結果を格納する
+            $new_results[$value[$primary_key]] = $value;
+
+        }
+
+        return $new_results;
+
+
+    }
+
 }

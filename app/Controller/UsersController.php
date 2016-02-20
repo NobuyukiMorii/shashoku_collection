6b1e97d36f0cc750fc90a53b1ce311c35a4956e1
Tamiiy <rashifamido@gmail.com>
2016-02-20 18:23:48 +0900

    ログイン画面整形

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -14,6 +14,8 @@ class UsersController extends AppController {
 
     }
 
+    public $layout = "sample";
+
     /**
      * ログイン
      * @return void
@@ -56,7 +58,7 @@ class UsersController extends AppController {
     }
 
     public function logout() {
-
+        $this->Auth->logout();
     }
 
     public function detail() {
